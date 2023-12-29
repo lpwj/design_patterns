dc4b540d4ec661639af5e886808a148de5dea396
lpwj <learnprogrammingwithjoel@gmail.com>
2023-12-29 15:24:56 +0000

    ♻️

--- oop/strategy/strategy_tut.py
+++ oop/strategy/strategy_tut.py
@@ -61,9 +61,12 @@ class PaymentContext:
 # Example usage
 context = PaymentContext(CreditCard())
 context.pay(100)  # Pay 100 with credit card
+
 context.set_payment(DebitCard())
 context.pay(200)  # Pay 200 with debit card
+
 context.set_payment(PayPal())
 context.pay(300)  # Pay 300 with PayPal
+
 context.set_payment(Cash())
 context.pay(400)  # Pay 400 with cash
