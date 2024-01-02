74d559874355a36096fec863ca12139c242e9761
lpwj <learnprogrammingwithjoel@gmail.com>
2024-01-02 11:42:28 +0000

    ✏️

--- functional/strategy/strategy_tut.py
+++ functional/strategy/strategy_tut.py
@@ -20,7 +20,7 @@ def debit_card(amount: float):
     print(f"Pay {amount} with debit card (Strategy Functional)")
 
 
-def pay_pal(amount: float):
+def paypal(amount: float):
     print(f"Pay {amount} with PayPal (Strategy Functional)")
 
 
@@ -31,5 +31,5 @@ def cash(amount: float):
 # Example usage
 pay(credit_card, 100)  # Pay 100 with credit card
 pay(debit_card, 200)  # Pay 200 with debit card
-pay(pay_pal, 300)  # Pay 300 with PayPal
+pay(paypal, 300)  # Pay 300 with PayPal
 pay(cash, 400)  # Pay 400 with cash
